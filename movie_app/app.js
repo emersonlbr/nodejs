897935a19c75698e35835657dd55b18f0c7ff747
emersonlbr <emersonlbr@icloud.com>
2017-05-06 20:56:21 -0300

    movie app complited

--- movie_app/app.js
+++ movie_app/app.js
@@ -5,18 +5,27 @@ var request = require("request");
 // setting the app to the return a html with ejs
 app.set("view engine", "ejs");
 
-
+app.get("/", function(req, res){
+  // rendering the search.ejs on the views folder
+  res.render("search");
+});
 
 // this is the route
 app.get("/results", function(req, res){
-    request("http://omdbapi.com/?s=iowa", function(error, response, body){
+    // getting the data from the user in the search
+    var query = req.query.search
+    // sending the info to the search api
+    var url = "http://omdbapi.com/?s=" + query;
+    console.log(query);
+
+    request(url, function(error, response, body){
       if(!error && response.statusCode == 200){
         // converting the api (wich at first is a string into a JSON element)
         var data = JSON.parse(body);
         // passing the the data to the template
         // it will have the same name
+        // rendering the results.ejs on the views folder
         res.render("results", {data: data});
-
       };
     });
 });
